53443fea9f32852b0811b994a53b0cbcd5ba099c
blabbermoutherish@gmail.com <blabbermoutherish@gmail.com@0f4755f0-7d2c-67f7-dad4-feab2342575f>
2011-07-09 19:41:20 +0000

    Initial Commit

---
+++ POAndroid/src/com/pokebros/android/pokemononline/POAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.pokebros.android.pokemononline;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class POAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
