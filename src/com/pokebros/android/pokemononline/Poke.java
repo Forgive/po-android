0d0e340ee8cc92641dd157a1561c89cc33c26fda
enanderle@gmail.com <enanderle@gmail.com@0f4755f0-7d2c-67f7-dad4-feab2342575f>
2011-07-09 21:28:13 +0000

    Initial commits of various data structures.

--- src/com/pokebros/android/pokemononline/Poke.java
+++ src/com/pokebros/android/pokemononline/Poke.java
@@ -1,5 +1,20 @@
 package com.pokebros.android.pokemononline;
 
+class UniqueID {
+	short pokeNum;
+	byte subNum;
+}
+
 public class Poke {
-	//HARP DARP
+	protected
+		UniqueID uID;
+		byte gen;
+		String nick;
+		short item;
+		short ability;
+		byte nature;
+		byte gender;
+		boolean shiny;
+		byte happiness;
+		byte level;
 }
---
+++ src/com/pokebros/android/pokemononline/Team.java
@@ -0,0 +1,7 @@
+package com.pokebros.android.pokemononline;
+
+public class Team {
+	protected
+		Poke[] pokes;
+		byte gen;
+}
