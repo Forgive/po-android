6c565323593a66d82ff00c3eefa7393bda6ecfb3
blabbermoutherish@gmail.com <blabbermoutherish@gmail.com@0f4755f0-7d2c-67f7-dad4-feab2342575f>
2011-07-16 14:38:51 +0000

    Started work on parsing XML files to read in custom teams

--- src/com/pokebros/android/pokemononline/PokeParser.java
+++ src/com/pokebros/android/pokemononline/PokeParser.java
@@ -1,35 +1,66 @@
 package com.pokebros.android.pokemononline;
-import java.io.IOException;
-import java.io.StringReader;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 public class PokeParser
 {
-
-    public static void main (String args[])
-        throws XmlPullParserException, IOException
-    {
-        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        XmlPullParser xpp = factory.newPullParser();
-
-        xpp.setInput( new StringReader ( "<foo>Hello World!</foo>" ) );
-        int eventType = xpp.getEventType();
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-         if(eventType == XmlPullParser.START_DOCUMENT) {
-             System.out.println("Start document");
-         } else if(eventType == XmlPullParser.START_TAG) {
-             System.out.println("Start tag "+xpp.getName());
-         } else if(eventType == XmlPullParser.END_TAG) {
-             System.out.println("End tag "+xpp.getName());
-         } else if(eventType == XmlPullParser.TEXT) {
-             System.out.println("Text "+xpp.getText());
-         }
-         eventType = xpp.next();
-        }
-        System.out.println("End document");
-    }
+	String nick, info, loseMsg, winMsg, defaultTier;
+	short avatar;
+	boolean ladderEnabled, showTeam;
+	byte gen;
+	short pokeNum;
+	byte subNum;
+	String pokenick;
+	short item, ability;
+	byte nature, gender;
+	boolean shiny;
+	byte happiness, level;
+	short[] moves;
+	byte[] DVs;
+	byte[] EVs;
+	
+	public String getNick() {return nick;}
+	public String getInfo() {return info;}
+	public String getLoseMsg() {return loseMsg;}
+	public String getWinMsg() {return winMsg;}
+	public String getDefaultTier() {return defaultTier;}
+	public short getAvatar() {return avatar;}
+	public boolean getLadderEnabled() {return ladderEnabled;}
+	public boolean getShowTeam() {return showTeam;}
+	public byte getGen() {return gen;}
+	public short pokeNum() {return pokeNum;}
+	public byte subNum() {return subNum;}
+	public String getPokeNick() {return pokenick;}
+	public short getItem() {return item;}
+	public short getAbility() {return ability;}
+	public byte getNature() {return nature;}
+	public byte getGender() {return gender;}
+	public boolean getShiny() {return shiny;}
+	public byte getHappiness() {return happiness;}
+	public byte getLevel() {return level;}
+	public short[] getmoves() {return moves;}
+	public byte[] getDVs() {return DVs;}
+	public byte[] getEVs() {return EVs;}
+	
+	
+	public void setNick(String s) {nick = s;}
+	public void setInfo(String s) {info = s;}
+	public void setLoseMsg(String s) {loseMsg = s;}
+	public void setWinMsg(String s) {winMsg = s;}
+	public void setDefaultTier(String s) {defaultTier = s;}
+	public void setAvatar(short s) {avatar = s;}
+	public void setLadderEnabled(boolean b) {ladderEnabled = b;}
+	public void setShowTeam(boolean b) {showTeam = b;}
+	public void setGen(byte b) {gen = b;}
+	public void setPokeNum(short s) {pokeNum = s;}
+	public void setSubNum(byte b) {subNum = b;}
+	public void setPokeNick(String s) {pokenick = s;}
+	public void setItem(short s) {item = s;}
+	public void setAbility(short s) {ability = s;}
+	public void setNature(byte b) {nature = b;}
+	public void setGender(byte b) {gender = b;}
+	public void setShiny(boolean b) {shiny = b;}
+	public void setHappiness(byte b) {happiness = b;}
+	public void setLevel(byte b) {level = b;}
+	public void setMoves(short[] s) {for (int i = 0; i < 4; ++i) {moves[i] = s[i];}}
+	public void setDVs(byte[] b) {for (int i = 0; i < 4; ++i) {DVs[i] = b[i];}}
+	public void setEVs(byte[] b) {for (int i = 0; i < 4; ++i) {EVs[i] = b[i];}}
 }
