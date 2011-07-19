b8319f471e40a6d5dd6a8b8b4a0e777eb055a590
billdh16@gmail.com <billdh16@gmail.com@0f4755f0-7d2c-67f7-dad4-feab2342575f>
2011-07-19 21:52:13 +0000

    Changed some IDs, strings, and removed some libraries that were unused

--- src/com/pokebros/android/pokemononline/ChatActivity.java
+++ src/com/pokebros/android/pokemononline/ChatActivity.java
@@ -10,7 +10,6 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
-import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
