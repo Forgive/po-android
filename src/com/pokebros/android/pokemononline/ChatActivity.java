7b73deed6fe4867ade8fd63c04ff66522ae638d3
mikegroshans@gmail.com <mikegroshans@gmail.com@0f4755f0-7d2c-67f7-dad4-feab2342575f>
2011-07-19 06:09:53 +0000

    chatBox now scrolls automatically. Might be worth only auto scrolling if the user is already at the bottom, but nothing immediately struck me, so for now it just always scrolls.

--- src/com/pokebros/android/pokemononline/ChatActivity.java
+++ src/com/pokebros/android/pokemononline/ChatActivity.java
@@ -10,17 +10,26 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ChatActivity extends Activity {
 	private NetworkService netServ = null;
+	private ScrollView chatScroll;
+	private TextView chatBox;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-        	TextView chatBox = (TextView) findViewById(R.id.chatBox);
-        	if (msg.getData().containsKey("ChannelMessage") && chatBox != null) {
+        	if (msg.getData().containsKey("ChannelMessage")) {
         		chatBox.append(msg.getData().getString("ChannelMessage") + "\n");
+            	chatScroll.post(new Runnable() {
+            		public void run() {
+            			chatScroll.fullScroll(View.FOCUS_DOWN);
+            		}
+            	});
         	}
         }
     };
@@ -43,7 +52,9 @@ public class ChatActivity extends Activity {
 		System.out.println("CREATED");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat);
-
+        chatScroll = (ScrollView) findViewById(R.id.chatScroll);
+    	chatBox = (TextView) findViewById(R.id.chatBox);
+        
         Intent intent = new Intent(ChatActivity.this, NetworkService.class);
         intent.putExtra("Messenger", messenger);
         intent.putExtra("Type", "chat");
